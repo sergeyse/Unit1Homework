472c57e203680ba418f0f5e3e60f8823322bda67
sergeyse <sergiy@simnet.is>
2014-02-25 23:09:14 +0000

    Homework Unit1 Click event handler

---
+++ v5/assets/js/mainM.js
@@ -0,0 +1,51 @@
+/**
+ * Created by sergeyse on 25.02.14.
+ */
+
+
+//---------- JavaScript ---------//
+window.onload= function(){ (function () {
+ var searchButton = document.getElementById("searchButton");
+ var featuredCarousel = document.getElementById('carousel-to-hide');
+ var searchBlock = document.getElementById('hidden-search');
+    var buttonB2=document.getElementById('b2');
+    var nawBar=document.getElementsByClassName('container');
+    buttonB2.addEventListener('click',hideCarousel,false);
+
+
+ //searchButton.addEventListener('click',hideCarousel,true);
+//    buttonB2.addEventListener('click',hideCarousel);
+ function hideCarousel(){
+ featuredCarousel.style.display="none";
+     searchBlock.style.display="block";
+
+ }
+
+ })();
+}
+//------------ JQuery example-----------//
+
+/*(function($){
+ $('#searchButton').on('click', showHide);
+ function showHide(){
+ $('#carousel-to-hide').toggle();
+
+
+ };
+ })(jQuery());*/
+/*
+$(function ($) {
+    $('#hidden-search').toggle();
+   */
+/* $('#searchButton').on('click', showHide);
+    function showHide() {
+        $('#carousel-to-hide').toggle();
+        $('#hidden-search').toggle();
+*//*
+
+
+    };
+
+    $('.slider').slider('min', 10);
+);
+*/
