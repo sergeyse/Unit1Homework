d307668bbc664700cd193415b6d2a1db7e91a4ca
sergeyse <sergiy@simnet.is>
2014-03-01 17:53:26 +0000

    Slider Input field Datepicker need a fix

---
+++ v5/Readme.txt
@@ -0,0 +1,1 @@
+Homework for Unit 1
--- v5/assets/js/mainM.js
+++ v5/assets/js/mainM.js
@@ -30,33 +30,82 @@
  })();
 }*/
 //------------ JQuery example-----------//
-$("#b2").click(function(){
-    $("#carousel-to-hide").slideToggle("fast");
-    console.log("finish");
-    $("#hidden-search").slideToggle("slow");
+      /*  avoiding page refresh*/
+$(document).ready(function () {
+    $("form").submit(function (e) {
+        e.preventDefault(); //prevent default form submit
+
+    });
 });
 
-/*(function($){
- $('#searchButton').on('click', showHide);
- function showHide(){
- $('#carousel-to-hide').toggle();
+
+jQuery(function($){
 
 
- };
- })(jQuery());*/
-/*
-$(function ($) {
-    $('#hidden-search').toggle();
-   */
-/* $('#searchButton').on('click', showHide);
-    function showHide() {
-        $('#carousel-to-hide').toggle();
-        $('#hidden-search').toggle();
-*//*
+//   -------------------- priseSlider-----------//
+
+    $('#priceSlider').slider().on("slide",function(e){
+        $('#price-start').val(e.value[0]);
+        $('#price-end').val(e.value[1]);
+    });
+
+    $('#datepicker').datepicker({
+        startDate: "-1d",
+        todayBtn: "linked",
+        todayHighlight: true
+    });
 
 
-    };
+                         /*-----setting up a global error handler */
+    $(function() {
+        $.ajaxSetup({
+            error: function(jqXHR, exception) {
+                if (jqXHR.status == 404) {
+                    alert('Requested resource not found. [404]');
+                } else if (jqXHR.status == 500) {
+                    alert('Internal Server Error [500].');
+                } else if (exception === 'parsererror') {
+                    alert('JSON parsing failed.');
+                } else {
+                    alert('Got This Error:\n' + jqXHR.responseText);
+                }
+            }
+        });
+    });
 
-    $('.slider').slider('min', 10);
-);
-*/
+           /*----retreaving json data and iterating it like A JS literal Object ----*/
+function loadJsonData(dataURL,rootElement,target) {
+    $.getJSON(dataURL, function (data) {
+        var optionsHTML = '';
+        $.each(data[rootElement], function(index) {
+            optionsHTML+='<p>'+data[rootElement][index].moto+' ! '+data[rootElement][index].todo+' '+
+                data[rootElement][index].message+'</p>'
+        });
+        var targetCurrentHTML = target.html();
+        var targetNewHTML = targetCurrentHTML + optionsHTML;
+        target.html(targetNewHTML);
+    });
+
+}
+
+    /*------- event hendlers---------*/
+/*-----------remember a css selector  display:none for #hidden-search------*/
+
+$("#searchButton").click(function(){
+        $("#carousel-to-hide").slideToggle();
+        console.log("finish");
+        $("#hidden-search").slideToggle();
+        loadJsonData('assets/data/products.json','content',$('#msg'));
+        $("#searchButton").hide();
+    });
+
+    $('#back-home').click(function(){
+        $("#carousel-to-hide").slideToggle();
+        console.log("finish");
+        $("#hidden-search").slideToggle();
+        $("#searchButton").show();
+        $("#msg p").remove();
+
+
+    });
+});
